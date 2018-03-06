c1d3a4213d1b70c6f44397d8b7bc560b2ee1cd06
Lisa Shepard <lisashepard04@gmail.com>
2018-03-06 12:04:11 -0500

    twittering

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require( 'express' ); 
 const app = express();  //create server 
+const nunjucks = require('nunjucks');
 
 app.use(function(req, res, next) {
     // log each request to the console
@@ -9,12 +10,33 @@ app.use(function(req, res, next) {
     next(); 
 });
 
+const localVars = {
+    title: 'Example Title',
+    people: [
+        {name: 'Alisa'},
+        {name: 'Lisa'},
+        {name: 'Harry'},
+        {name: 'Bobby'},
+    ]
+};
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
 app.get('/', function(req, res){
-    res.send('Hey there!')
-})
+    res.render('index.html', localVars);
+});
+
+// const port = 3000;
+// app.listen(port, function(){
+//     console.log(`we are listening on ${port}`);
+// })
+
+nunjucks.configure('views', {noCache: true});
+// nunjucks.render('index.html', localVars, function(err, res) {
+//     console.log('hello', res);
+// });
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
 
-const port = 3000;
-app.listen(port, function(){
-    console.log(`we are listening on ${port}`);
-})
+// const people = [{name: 'Mary'}, {name: 'Steve'}, {name: 'Jack'}];
+// res.render('index', {title: 'Title', people: people});
